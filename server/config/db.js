7510e8c25a929c8a92bb42b22a27a0a1deee82f3
fuhzaken <janilpandak132@gmail.com>
2020-10-30 19:12:50 -0400

    added my files

---
+++ server/config/db.js
@@ -0,0 +1,11 @@
+/*
+File Name: db.js
+Author: Kenneth Agustin
+StudentID: 301040904
+Web App Name: Favourite Books List
+*/
+module.exports = {
+  //local MongoDB deployment ->
+  //"URI": "mongodb://localhost/books229"
+  "URI": "mongodb+srv://kinet:eO5KuQfNclODc5mc@mongodbserver.w56bk.mongodb.net/book_list?retryWrites=true&w=majority"
+};
